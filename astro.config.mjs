678588a56d759f1b1e995e2bec4ef8ac352ec713
O6lvl4 <hiromi.motodera@aid-on.org>
2026-03-27 07:47:37 +0900

    Set Almide logo as favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig({
 				},
 			},
 			title: 'Almide',
+			favicon: '/favicon.png',
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/almide/almide' },
 			],
